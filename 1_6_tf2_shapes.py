1aeef8614d3944cbfe29a8ca2478d9bea8c3e3d4
alvarohernandezalt <alvarohernandezalt@gmail.com>
2023-07-09 18:49:05 +0200

    Add 1_6 patch

---
+++ 1_6_tf2_shapes.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+list_0 = []
+tuple_0 = ()
+print('list_0: ', list_0)
+print('tuple_0: ',tuple_0)
+
+list_1 = [3]
+tuple_1 = (3)
+print('list_1: ', list_1)
+print('tuple_1: ',tuple_1)
+
+list_2 = [3, 7]
+tuple_2 = (3, 7)
+print('list_2: ', list_2)
+print('tuple_2: ',tuple_2)
