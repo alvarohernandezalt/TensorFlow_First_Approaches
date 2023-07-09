78faace56170deadb99c223e12bfc5954c4dcf29
alvarohernandezalt <alvarohernandezalt@gmail.com>
2023-07-09 11:29:02 +0200

    get_shape patch

---
+++ 1_5_tf2_getshape.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+
+a = tf.constant(3.0)
+print ('a shape: ', a.get_shape())
+
+b = tf.fill([2,3],5.0)
+print('b shape: ', b.get_shape())
+
+c= tf.constant([[1.0,2.0,3.0],[4.0,5.0,6.0]])
+print('c shape: ', c.get_shape())
