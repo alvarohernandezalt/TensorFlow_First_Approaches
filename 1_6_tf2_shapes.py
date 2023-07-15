47339ce6f5cf630065442839389bb6aca1996acb
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-15 13:01:54 +0200

    Update 1_6_tf2_shapes.py
    Finish and add comment

--- 1_6_tf2_shapes.py
+++ 1_6_tf2_shapes.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 
+# Simple list and tuples of varous dimensions. 
+# To ilustrate the difference between them
+
 list_0 = []
 tuple_0 = ()
 print('list_0: ', list_0)
@@ -14,3 +17,14 @@ list_2 = [3, 7]
 tuple_2 = (3, 7)
 print('list_2: ', list_2)
 print('tuple_2: ',tuple_2)
+
+any_list1 = [None]
+any_tuple1 = {None}
+print('any_list1: ',any_list1)
+print('any_tuple1: ',any_tuple1)
+
+any_list2 = [7,None]
+any_list3 = [7,None,None]
+print('any_list2: ',any_list1)
+print('any_list3: ',any_tuple1)
+
