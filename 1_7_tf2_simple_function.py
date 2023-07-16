2900c5041c2e47cba75f110923608dffd484e2c9
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-16 09:38:40 +0200

    1_7 looking bad code

--- 1_7_tf2_simple_function.py
+++ 1_7_tf2_simple_function.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 def func():
-    a = tf.constant ([[10,10],[11.,1.]])
+    a = tf.constant ([[10.,10.],[11.,1.]])
     b = tf.constant ([[1.,0.],[0.,1.]])
     c = tf.matmul(a,b)
     return c
