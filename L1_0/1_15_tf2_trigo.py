83925b77dc4d4b08c5128967c1d018bd6ee929de
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-23 11:10:19 +0200

    Create 1_15_tf2_trigo.py

---
+++ L1_0/1_15_tf2_trigo.py
@@ -0,0 +1,18 @@
+import tensorflow as tf 
+import math as m 
+
+PI = tf.constant(m.pi)
+
+a = tf.cos(PI/3.)
+b = tf.sin(PI/3.)
+c = 1.0/a #sec(60)
+d = 1.0/tf.tan(PI/3.) # cot(60)
+
+@tf.function # this decorator is ok 
+def math_values():
+    print("a: ", a)
+    print("b: ", b)
+    print("c: ", c)
+    print("d: ", d)
+
+math_values()
