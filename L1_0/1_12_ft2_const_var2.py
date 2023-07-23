0b9edd93ca0474a16a3b818270f32fa41d2f6b7c
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-23 10:33:04 +0200

    Fix 1_12.py a create .vscode

--- L1_0/1_12_ft2_const_var2.py
+++ L1_0/1_12_ft2_const_var2.py
@@ -4,4 +4,12 @@ v1 = tf.Variable([4.0, 4.0])
 c1 = tf.constant([1.0, 2.0])
 
 diff = tf.subtract(v1, c1)
-print('Diff: ', diff.numpy())
+print("Diff1: ", diff.numpy())
+
+# diff is NOT updated
+v1.assign([10.0, 20.0])
+print("Diff2: ", diff.numpy())
+
+# diff updated
+diff = tf.subtract(v1, c1)
+print("Diff3: ", diff.numpy())
