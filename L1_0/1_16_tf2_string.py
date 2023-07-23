9b59f281877f957fafcef4e792556470a0e507ba
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-23 11:43:31 +0200

    Update 1_16_tf2_string.py

--- L1_0/1_16_tf2_string.py
+++ L1_0/1_16_tf2_string.py
@@ -4,3 +4,11 @@ x1 = tf.constant('café')
 print("x1: ", x1)
 tf.strings.length(x1)
 print ("")
+
+len1 = tf.strings.length(x1, unit="UTF8_CHAR")
+len2 = tf.strings.unicode_decode(x1, "UTF8")
+
+print("len1: ",len1.numpy())
+print("len1: ",len2.numpy())
+print("")
+
