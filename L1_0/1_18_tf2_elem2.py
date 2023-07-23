7035f0558a5325ecb5ba941cc932ad4a930bcf49
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-23 20:00:15 +0200

    Update 1_18_tf2_elem2.py

--- L1_0/1_18_tf2_elem2.py
+++ L1_0/1_18_tf2_elem2.py
@@ -7,6 +7,11 @@ def compute_values():
     tf.print('arr2: ',arr2)
     arr_0 = arr2[0]
     tf.print('[0]: ',arr_0)
-    print('[1]: ',arr2[1]) # Inside def, normal print in "eager" mode is always the abstraction of the tensor as result
+    print('[1]: ',arr2[1]) 
+
+# Inside def, normal print in "eager" mode is always 
+# the abstraction of the tensor as result
+# And in the case of using tf.print() works inside a "def" 
+# the same as if we use print(arr2.numpy()) outside tf.function
 
 compute_values()
