4944015b0c4afbdb23d6b6c95d13acb9f5b92bb5
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-23 17:57:12 +0200

    Update 1_16_tf2_string.py

--- L1_0/1_16_tf2_string.py
+++ L1_0/1_16_tf2_string.py
@@ -9,7 +9,7 @@ len1 = tf.strings.length(x1, unit="UTF8_CHAR")
 len2 = tf.strings.unicode_decode(x1, "UTF8")
 
 print("len1: ",len1.numpy())
-print("len1: ",len2.numpy())
+print("len2: ",len2.numpy())
 print("")
 
 # string arrays
@@ -18,7 +18,7 @@ print('x2: ', x2)
 print("")
 
 len3 = tf.strings.length(x2, unit="UTF8_CHAR")
-print('len2: ', len3.numpy())
+print('len3: ', len3.numpy())
 print("")
 
 r = tf.strings.unicode_decode(x2, "UTF8")
