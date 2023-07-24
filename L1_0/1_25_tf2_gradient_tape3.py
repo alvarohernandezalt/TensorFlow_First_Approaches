220b3b49800d1562352aa0eca0d48e108db7a958
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-24 12:44:54 +0200

    Update 1_25_tf2_gradient_tape3.py

--- L1_0/1_25_tf2_gradient_tape3.py
+++ L1_0/1_25_tf2_gradient_tape3.py
@@ -14,4 +14,12 @@ dz2_dx2 = t1.gradient(dz_dx, x)
 print('First dz_dx: ', dz_dx)
 print('Second dz2_dx2: ', dz2_dx2)
 
+x = tf.Variable(4.0)
+with tf.GradientTape() as t1:
+    with tf.GradientTape() as t2:
+        z = x * x * x
+    dz_dx = t2.gradient(z, x)
+dz2_dx2 = t1.gradient(dz_dx, x)
 
+print('First dz_dx: ', dz_dx)
+print('Second dz2_dx2: ', dz2_dx2)
