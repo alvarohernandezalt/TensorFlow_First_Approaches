d2252eaae1208d05fd8f2d9b0ae156d38f2f38fe
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-26 11:03:04 +0200

    Update 2_7_tf2_range1.py

--- L2_0/2_7_tf2_range1.py
+++ L2_0/2_7_tf2_range1.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 
+
+# tf.range takes an initial number and a final one. 
+# Generate an array of the values that goes from init to end counted by the third number/variable
+
 a1 = tf.range(3,18,3)
 b2 = tf.range(1,133,11)
 
