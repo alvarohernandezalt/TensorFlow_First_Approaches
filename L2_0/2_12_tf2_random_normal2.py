829e72459f6214113cb82953645c2fc6fa172a8b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-26 17:24:59 +0200

    Update 2_12_tf2_random_normal2.py

--- L2_0/2_12_tf2_random_normal2.py
+++ L2_0/2_12_tf2_random_normal2.py
@@ -6,3 +6,14 @@ for i in range(3):
     print('x_train: ',x_train)
 print('----------------------\n')
 
+for i in range(3):
+    x_train = tf.random.normal((2,), mean=5, stddev=2.0)
+    y_train = x_train * 2 + 4
+    print('x_train: ',x_train)
+print('----------------------\n')
+
+for i in range(3):
+    x_train = tf.random.normal((3,), mean=5, stddev=2.0)
+    y_train = x_train * 2 + 6
+    print('x_train: ',x_train)
+print('----------------------\n')
