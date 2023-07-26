8668321368ff4470039e637852456ee2cc9d6392
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-26 16:13:53 +0200

    Create 2_10_ft2_reduce_mean.py

---
+++ L2_0/2_10_ft2_reduce_mean.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+x = tf.constant([100,200,300], name = 'x')
+y = tf.constant([1,2,3], name='y')
+
+sum_x = tf.reduce_sum(x, name="sum_x")
+prod_y = tf.reduce_prod(y, name='prod_y')
+mean = tf.reduce_mean([sumx,prod_y], name="mean")
+
+print("sum_x: ", sum_x)
+print("prod_y: ", prod_y)
+print("mean: ", mean)
