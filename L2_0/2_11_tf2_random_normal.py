28858ca4e2410241cf258a0eb5bf627a533ee17b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-26 16:30:47 +0200

    Create 2_11_tf2_random_normal.py

---
+++ L2_0/2_11_tf2_random_normal.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+# initialize a 6x3 2nd order tensor of random numbers
+values = {'weights':tf.Variable(tf.random.normal([6,3]))}
+
+print('Values:')
+print(vaues['weights'])
