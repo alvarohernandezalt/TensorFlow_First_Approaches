630db6b024fea2cd64fe2f13fc51ef99c02a78f8
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-26 15:26:40 +0200

    Update 2_9_tf2_rerduce_prod.py

--- L2_0/2_9_tf2_rerduce_prod.py
+++ L2_0/2_9_tf2_rerduce_prod.py
@@ -4,3 +4,10 @@ x = tf.constant([100,200,300], name = 'x')
 y = tf.constant([1,2,3], name='y')
 
 sum_x = tf.reduce_sum(x, name="sum_x")
+prod_y = tf.reduce_prod(y, name='prod_y')
+div_xy = tf.math.divide(sum_x, prod_y, name="div_xy")
+
+print("sum_x: ", sum_x)
+print("prod_y: ", prod_y)
+print("div_xy: ", div_xy)
+
