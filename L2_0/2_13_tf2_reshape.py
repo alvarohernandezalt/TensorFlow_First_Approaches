48c5029e6752bc0cb6ca6c6b2f03a01bbc20e7f2
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-27 13:16:24 +0200

    Update 2_13_tf2_reshape.py

--- L2_0/2_13_tf2_reshape.py
+++ L2_0/2_13_tf2_reshape.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 x = tf.constant([[2,5,3,-5],[0,3,-2,5],[4,3,5,3]])
 
 print("shape: ", tf.shape(input=x))
-print("shape: ", tf.shape(x, [6,2]))
-print("shape: ", tf.shape(x, [3,4]))
+print("shape: ", tf.reshape(x, [6,2]))
+print("shape: ", tf.reshape(x, [3,4]))
