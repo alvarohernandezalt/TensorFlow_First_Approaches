5d4e9a61e8fba9d3ee1539ffa4a18031e9353d2c
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-28 10:09:27 +0200

    Update 2_17_tf2_argmax.py

--- L2_0/2_17_tf2_argmax.py
+++ L2_0/2_17_tf2_argmax.py
@@ -1,3 +1,10 @@
 import tensorflow as tf
 import numpy as np
 
+x1 = tf.constant([3.9, 2.1, 2.3, -4.0])
+x2 = tf.constant([1.0, 2.0, 5.0, -4.2])
+
+print('x1: ',x1)
+print('x2: ',x2)
+print('a1: ',tf.argmax(input=x1, axis=0))
+print('a2: ',tf.argmax(input=x2, axis=0))
