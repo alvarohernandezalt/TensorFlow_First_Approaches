6c33498f7bee3f02fe4052e1c7d94a2016988a10
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-31 12:13:50 +0200

    Update 2_22_tf2_map_function.py

--- L2_0/2_22_tf2_map_function.py
+++ L2_0/2_22_tf2_map_function.py
@@ -9,3 +9,10 @@ print('doubles: ', doubles)
 squares = tf.map_fn(lambda x : x * x, elems)
 print('squares: ', squares)
 
+elems = (np.array([1,2,3]), np.array([-1,1,-1]))
+neg_pos = tf.map_fn(lambda x: x[0] * x[1], elems, dtype=tf.int64)
+print('neg_pos: ', neg_pos)
+
+elems = np.array([1,2,3])
+pos_neg = tf.map_fn(lambda x: (x, -x), elems, dtype =(tf.int64, tf.int64))
+print('pos_neg: ', pos_neg)
