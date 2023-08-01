cbb638afb33d3a925effda4b9a6c1b2cbc9f1f4b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-01 12:28:52 +0200

    Update 2_25_tf2_ragged_tensors1.py

--- L2_0/2_25_tf2_ragged_tensors1.py
+++ L2_0/2_25_tf2_ragged_tensors1.py
@@ -8,3 +8,8 @@ digits = tf.ragged.constant([[3, 1,4,1], [], [5,9,2], [6],[]])
 words = tf.ragged.constant([['Bye', 'now'], ['Thank', 'you', 'again', 'sir']])
 
 print(tf.add(digits, 3))
+print(tf.reduce_mean(digits, axis=0))
+print(tf.concat([digits, [[5, 3]]], axis=0))
+print(tf.tile(digits, [1,2]))
+print(tf.strings.substr(words, 0, 2))
+
