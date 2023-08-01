1218de5a7f209545f600b33bce7f97eb1a3c270b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-01 19:35:30 +0200

    Update 2_27_tf2_record1.py

--- L2_0/2_27_tf2_record1.py
+++ L2_0/2_27_tf2_record1.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 data = b'pasta'
 simple1 = tf.train.Example(features=tf.train.Feature(
     feature={
-        'my_ints' : tf.train.Feature(int66_list=tf.train.Int64List(value=[2, 5])),
+        'my_ints' : tf.train.Feature(int64_list=tf.train.Int64List(value=[2, 5])),
         'my_float': tf.train.Feature(float_list=tf.train.FloatList(value=[ 3.6])),
         'my_bytes': tf.train.Feature(bytes_list=tf.train.BytesList(value=[data]))
     }
