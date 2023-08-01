2f2fa2f2496ee780a448b431a84850084716872b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-01 19:44:52 +0200

    Update 2_27_tf2_record1.py

--- L2_0/2_27_tf2_record1.py
+++ L2_0/2_27_tf2_record1.py
@@ -3,12 +3,17 @@ import tensorflow as tf
 data = b'pasta'
 simple1 = tf.train.Example(features=tf.train.Features(
     feature={
-        'my_ints' : tf.train.Feature(int64_list=tf.train.Int64List(value=[2, 5])),
-        'my_float': tf.train.Feature(float_list=tf.train.FloatList(value=[ 3.6])),
-        'my_bytes': tf.train.Feature(bytes_list=tf.train.BytesList(value=[data]))
+        'my_ints' : tf.train.Feature(int64_list = tf.train.Int64List(value=[2, 5])),
+        'my_float': tf.train.Feature(float_list = tf.train.FloatList(value=[ 3.6])),
+        'my_bytes': tf.train.Feature(bytes_list = tf.train.BytesList(value=[data]))
     }
 ))
+my_ints_value = simple1.features.feature['my_ints'].int64_list.value
+print('my_ints:  ', simple1.features.feature['my_ints'], my_ints_value)
 
-print('my_ints: ', simple1.features.feature['my_ints'], int64_list.value)
-print('my_float: ', simple1.features.feature['my_float'], float_list.value)
-print('my_bytes: ', simple1.features.feature['my_bytes'], bytes_list.value)
+my_float_value = simple1.features.feature['my_ints'].float_list.value
+print('my_float: ', simple1.features.feature['my_float'], my_float_value)
+
+# print('my_bytes: ', simple1.features.feature['my_bytes'], bytes_list.value)
+
+print(('simple1: ', simple1))
