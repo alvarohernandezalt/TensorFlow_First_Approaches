1c81e88c2334571c0b873b9673ad926c41a11071
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-14 17:46:11 +0200

    Create 3_0_tf2_slices.py

---
+++ L3_0/3_0_tf2_slices.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+# combine the input into one element
+t1 = tf.constant([[1, 2],[3, 4]])
+ds1 = tf.data.Dataset.from _tensors(t1)
+
+# separate element for each item
+t2 = tf.constant([[1, 2],[3, 4]])
+ds2 = tf.data.Dataset.from_tensor_slices(t2)
+for item in ds1:
+    print("1item: ", item)
+
+print('--------------------------')
+
+for item in ds2:
+    print("1item: ", item)
