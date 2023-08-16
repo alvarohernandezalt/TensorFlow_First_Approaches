6dcec4c2f53a8f6383a1a81e859a13a5bca43529
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-16 10:40:52 +0200

    Change name and update 3_3

--- L3_0/3_3_tf2_concatenate.py
+++ L3_0/3_3_tf2_concatenate.py
@@ -7,3 +7,9 @@ x2 = np.array([6, 7, 8, 9, 10])
 ds1 = tf.data.Dataset.from_tensor_slices(x1)
 ds2 = tf.data.Dataset.from_tensor_slices(x2)
 ds3 = ds1.concatenate(ds2)
+
+try:
+    for value in ds3.take(20):
+        print('value: ', value)
+except tf.errors.OutOfRangeError:
+    pass
