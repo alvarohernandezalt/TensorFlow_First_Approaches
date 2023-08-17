37f7ef902fbe66c2857bdd744b1e57870407171e
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 12:39:50 +0200

    Update 3_11_tf2_mao2.py

--- L3_0/3_11_tf2_mao2.py
+++ L3_0/3_11_tf2_mao2.py
@@ -5,4 +5,18 @@ import numpy as np
 x = np.array([[1],[2],[3],[4]])
 
 # Make a dataset from a NumPy array
-datset = tf.data.Dataset.from_tensor_slices(x)
+dataset = tf.data.Dataset.from_tensor_slices(x)
+
+# METHOD #1: THE LONG WAY
+# a lambda expression to double each value
+# dataset = dataset.map(lambda x: x*2)
+# a lambda expression to add one
+# dataset = dataset.map(lambda x: x+1)
+# a lambda expression to cube each value
+# dataset = dataset.map(lambda x: x**3)
+
+# METHOD #2: THE SHORT WAY
+dataset = dataset.map(lambda x: ((2*x)+1)**3)
+
+for values in dataset:
+    print('values: ', values)
