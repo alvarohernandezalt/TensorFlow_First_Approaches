3685d767f3863f685ea60674696fcce3562cbaef
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 12:05:28 +0200

    Create 3_8_tf2_batch1.py

---
+++ L3_0/3_8_tf2_batch1.py
@@ -0,0 +1,12 @@
+import tensorflow as tf 
+import numpy as np
+
+'''
+The batch(n) operator returns tensor formed by n elements until we reach x-1
+Remember lists always start from 0 so (0,34) -> 0 to 33 (34 elements)
+'''
+x = np.arange(0,34)
+ds = data.Datset.from_tensor_slices(x).batch(3)
+
+for value in ds: 
+    print('value: ', value)
