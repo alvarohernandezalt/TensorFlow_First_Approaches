a42e940a6225bdff552c4f172b08bd4a94ae65f8
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 13:22:56 +0200

    Update 3_12_tf2_flatmap.py

---
+++ L3_0/3_12_tf2_flatmap.py
@@ -0,0 +1,17 @@
+''' 
+flat_map() takes a function that maps a single element of the input 
+dataset to a Dataset of elements
+
+'''
+
+import tensorflow as tf 
+import numpy as np 
+
+
+x = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
+
+ds = tf.data.Dataset.from_tensor_slices(x)
+ds.flat_map(lambda x: tf.dataDataset.from_tensor_slices(x))
+
+for value in ds:
+    print('value:',value)
