4573b8f78f6897ada52d710d9023b5b6ad23d1da
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 12:50:15 +0200

    Update 3_11_tf2_mao2.py

--- L3_0/3_11_tf2_mao2.py
+++ L3_0/3_11_tf2_mao2.py
@@ -15,16 +15,16 @@ dataset = tf.data.Dataset.from_tensor_slices(x)
 # a lambda expression to cube each value
 # dataset = dataset.map(lambda x: x**3)
 
-# METHOD #2: THE SHORT WAY
-dataset = dataset.map(lambda x: x*2).map(lambda x: x+1).map(lambda x: x**3)
+# METHOD #2: THE SHORT WAY(THE BOOK's WAY, with warnings)
+# dataset = dataset.map(lambda x: x*2).map(lambda x: x+1).map(lambda x: x**3)
 
-# METHOD #3: THE SHORTEST WAY (THE BOOK DON'T SAY ABOUT IT)
+# 👉👉 METHOD #3: THE SHORTEST WAY (THE BOOK DON'T SAY ABOUT IT)
 # Just doing some simple math inside the lambda expession
 dataset2 = dataset.map(lambda x: ((2*x)+1)**3)
 
 # testing and comparing that they are the same values
-for values in dataset:
-    print('values: ', values)
+#for values in dataset:
+#    print('values: ', values)
 
 for values in dataset2:
     print('values: ', values)
