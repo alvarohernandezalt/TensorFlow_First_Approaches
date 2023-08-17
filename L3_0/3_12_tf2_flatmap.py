3c365a816a69d081b0576447bf9f9def0b648372
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 13:32:17 +0200

    Update 3_12_tf2_flatmap.py

--- L3_0/3_12_tf2_flatmap.py
+++ L3_0/3_12_tf2_flatmap.py
@@ -13,5 +13,11 @@ x = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
 ds = tf.data.Dataset.from_tensor_slices(x)
 ds.flat_map(lambda x: tf.data.Dataset.from_tensor_slices(x))
 
-for value in ds:
+# ds.flat_map apparently is not doing nothing 
+# if I comment it he ds values are the same 
+# also it does'nt matter to call "for value in ds" 
+# or "for value in ds.take(3), because 3 is the maximun elements
+# of the dataset"  
+
+for value in ds.take(3):
     print('value:',value)
