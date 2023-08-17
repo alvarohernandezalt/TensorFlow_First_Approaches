07c94c1f32bc7b0b372c52e16d22fc4e8b1a4950
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 09:35:57 +0200

    Create 3_6_tf2_filter.py

---
+++ L3_0/3_6_tf2_filter.py
@@ -0,0 +1,13 @@
+"""
+The filter() operator uses Boolean logic to "filter" the elements in an array,
+in order to determine which elements satisfy the Boolean condition.
+
+"""
+
+import tensorflow as tf 
+import numpy as np
+
+#def filter_fn(x):
+#   return tf.reshape(tf.not_equal(x % 2, 1), [])
+
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
