f97f5c5913fdde9d11c1319e244972e65771c867
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 12:06:20 +0200

    Update 3_8_tf2_batch1.py

--- L3_0/3_8_tf2_batch1.py
+++ L3_0/3_8_tf2_batch1.py
@@ -6,7 +6,7 @@ The batch(n) operator returns tensor formed by n elements until we reach x-1
 Remember lists always start from 0 so (0,34) -> 0 to 33 (34 elements)
 '''
 x = np.arange(0,34)
-ds = data.Datset.from_tensor_slices(x).batch(3)
+ds = tf.data.Datset.from_tensor_slices(x).batch(3)
 
 for value in ds: 
     print('value: ', value)
