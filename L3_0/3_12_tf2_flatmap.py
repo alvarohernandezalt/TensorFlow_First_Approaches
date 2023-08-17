cb7f974333c740deae26ab8a6f39678a5c76baca
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 13:23:45 +0200

    Update 3_12_tf2_flatmap.py

--- L3_0/3_12_tf2_flatmap.py
+++ L3_0/3_12_tf2_flatmap.py
@@ -11,7 +11,7 @@ import numpy as np
 x = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
 
 ds = tf.data.Dataset.from_tensor_slices(x)
-ds.flat_map(lambda x: tf.dataDataset.from_tensor_slices(x))
+ds.flat_map(lambda x: tf.data.Dataset.from_tensor_slices(x))
 
 for value in ds:
     print('value:',value)
