2319a9f00ffc3e6018ed66caa9c8cd0900d3c7f3
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 12:15:44 +0200

    Update 3_9_ft2_generator2.py

--- L3_0/3_9_ft2_generator2.py
+++ L3_0/3_9_ft2_generator2.py
@@ -14,3 +14,5 @@ ds = tf.data.Dataset.from_generator(gener, (tf.int64, tf.int64, tf.int64))
 third = int(len(x)/3)
 for value in ds.take(third):
     print('value: ', value)
+
+### NOTE: look also 3__5_tf2_generator.py and compare and experiment...
