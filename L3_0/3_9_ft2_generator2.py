86f8c4bb2847c428bf8d76a8f33395b1f79f9c89
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-17 12:11:39 +0200

    Create 3_9_ft2_generator2.py

---
+++ L3_0/3_9_ft2_generator2.py
@@ -0,0 +1,16 @@
+import tensorflow as tf 
+import numpy as np
+
+x = np.arange(0, 12)
+
+def gener():
+    i=0
+    while(i < len(x/3)):
+        yield (i, i+1, i+2)
+        i += 3
+
+ds = data.Dataset.from_generator(gener, (tf.int64, tf.int64, tf.int64))
+
+third = int(len(x)/3)
+for value in ds.take(third):
+    print('value: ', value)
