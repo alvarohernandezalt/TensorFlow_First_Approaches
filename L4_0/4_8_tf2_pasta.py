51125741548d05b8272f7cf52149afb6f3587d0f
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-21 18:15:50 +0200

    Create 4_8_tf2_pasta.py

---
+++ L4_0/4_8_tf2_pasta.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# price of pasta per kilogram
+df = pd.read_csv('4_7_pasta.csv')
+
+weight = df['weight']
+price = df['price']
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1, input_shape=[1])
+])
+
+# MSE loss function and Adam optimizer
+model.compile(loss='mean_squared_error',
+              optimizer=tf.keras.optimizers.adam(0.1))
+
+# Train the model
+history = model.fit(weight, price, epochs=100, verbose=False)
+
+# Graph the # of epochs versus the loss
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.plot(history.history['loss'])
+plt.show()
+
+print('Cost for 11kg: ', model.predict([11.0]))
+print('Cost for 45kg: ', model.predict([45.0]))
