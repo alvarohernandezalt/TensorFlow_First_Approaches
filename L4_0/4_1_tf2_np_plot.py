7e8475d96ec4c594fbfce9c63833d1e55c26e772
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-21 13:55:10 +0200

    Update 4_1_tf2_np_plot.py

--- L4_0/4_1_tf2_np_plot.py
+++ L4_0/4_1_tf2_np_plot.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 
 # Create a pseudo-random x,y values for plotting purposes.
 
+# Return smaples with shape(15,1) with standard normal distribution
 x = np.random.randn(15, 1)
+# Add pseudo randomness to y paired values 
 y = 2.5*x + 5 + 0.2*np.random.randn(15,1)
 
 print('x: ',x)
