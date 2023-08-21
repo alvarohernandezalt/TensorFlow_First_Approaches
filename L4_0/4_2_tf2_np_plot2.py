4692d07fc5207ab7801198251aa377d42d81c02d
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-21 14:04:51 +0200

    Update 4_2_tf2_np_plot2.py

--- L4_0/4_2_tf2_np_plot2.py
+++ L4_0/4_2_tf2_np_plot2.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# x Returns 11 evenly spaced samples, calculated over the interval [-1, 1].
+x_data = np.linspace(-1, 1, 11)
+# Create some perturbance in the y pairs for real regression model
+y_data = 4*x_data + np.random.randn(*x_data.shape)*0.5
+
