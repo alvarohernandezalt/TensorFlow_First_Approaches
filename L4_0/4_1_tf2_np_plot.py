26eae4703204b1e800ee891155a6f72f76ea290d
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-21 13:47:33 +0200

    Update 4_1_tf2_np_plot.py

--- L4_0/4_1_tf2_np_plot.py
+++ L4_0/4_1_tf2_np_plot.py
@@ -1,8 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Create a pseudo-random x,y values for plotting purposes.
+
 x = np.random.randn(15, 1)
 y = 2.5*x + 5 + 0.2*np.random.randn(15,1)
 
 print('x: ',x)
 print('y: ',y)
+
+plt.scatter(x,y)
+plt.show()
