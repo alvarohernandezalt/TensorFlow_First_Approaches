2f411f624b59d39735d5f5c29fee4bc22f67af92
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-22 13:53:55 +0200

    Update 4_8_tf2_pasta.py

--- L4_0/4_8_tf2_pasta.py
+++ L4_0/4_8_tf2_pasta.py
@@ -3,11 +3,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# price of pasta per kilogram
-df = pd.read_csv('4_7_pasta.csv', index_col=0)
+# Load data
+df = pd.read_csv('4_7_pasta.csv', index_col=0, encoding='utf-8')
 
-weight = df['weight']
-price = df['price']
+# Remove any leading/trailing whitespace from column names
+df.columns = df.columns.str.strip()
+
+# Access columns 'weight' and 'price'
+weight = df[['weight']]
+price = df[['price']]
+
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(1, input_shape=[1])
@@ -15,7 +20,7 @@ model = tf.keras.Sequential([
 
 # MSE loss function and Adam optimizer
 model.compile(loss='mean_squared_error',
-              optimizer=tf.keras.optimizers.adam(0.1))
+              optimizer=tf.keras.optimizers.Adam(0.1))
 
 # Train the model
 history = model.fit(weight, price, epochs=100, verbose=False)
